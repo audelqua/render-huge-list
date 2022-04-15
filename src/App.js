2a1844b8b7eea9d20f141c508788be2dcac26308
audrey <mbozorgi604@gmail.com>
2022-04-15 14:56:07 +0430

    add child component with heavy list to render. handled with react-window

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {useState, UseEffect, useEffect} from 'react'
+import myJSONData from './constants/data.json'
 import './App.css';
+import { FixedSizeList as List } from 'react-window';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeavyChildComponent />
     </div>
   );
 }
 
 export default App;
+
+const HeavyChildComponent = (props) => { 
+  // const [myData, updateMyData] = useState(myJSONData.human.filter((item , index) => index < 1000));
+  const [myData, updateMyData] = useState(myJSONData.human);
+  
+  return(
+    <List
+      height={1000}
+      itemSize={100}
+      width={'100%'}
+      itemData={myData}
+      itemCount={myData.length}
+    >
+      {({data, index, style}) => {
+        return (
+          <tr style={style}>
+            <td>{index}</td>
+          </tr>
+        )
+      }}
+    </List>
+  )
+}
+
+
