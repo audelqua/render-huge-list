0a3ee50de87ef2685bfbf83b2936c998c7da37e7
audrey <mbozorgi604@gmail.com>
2022-04-24 02:15:41 +0430

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,49 +1,66 @@
 import React, {useState, UseEffect, useEffect} from 'react'
-import myJSONData from './constants/data.json'
+// import myJSONData from './constants/data.json'
 import './App.css';
+import { useColorFleeter } from './hooks/useColorFleeter'
 import {AutoSizer, List} from 'react-virtualized';
 import styled from 'styled-components'
 import 'react-virtualized/styles.css'; // only needs to be imported once
 
+let timerUnifier; 
 function App() {
+  const [number, setNumber] = useState(0)
   
+  const randomNumberGenerator = () => {
+    setNumber(Math.floor(Math.random() * 100))
+  }
+  
+  useEffect(() => {
+    timerUnifier = setInterval(() => {
+      randomNumberGenerator()
+    }, 3000)
+    return () => {
+      clearInterval(timerUnifier)
+    }
+  }, [])
+
   return (
     <div className="App">
-      <HeavyChildComponent />
+      <div style={{background: useColorFleeter(number), height: '100vh'}}>
+        
+      </div>
     </div>
   );
 }
 
 export default App;
 
-const HeavyChildComponent = (props) => { 
-  const [myData, updateMyData] = useState(myJSONData.human.filter((item , index) => index < 1000));
-  // const [myData, updateMyData] = useState(myJSONData.human);
+// const HeavyChildComponent = (props) => { 
+//   // const [myData, updateMyData] = useState(myJSONData.human.filter((item , index) => index < 1000));
+//   // const [myData, updateMyData] = useState(myJSONData.human);
 
-
-  function rowRenderer({key, index, style}) {
-    return (
-      <div key={key} style={style}>
-        {myData[index]}
-      </div>
-    );
-  }
+//   function rowRenderer({key, index, style}) {
+//     return (
+//       <div key={key} style={style}>
+//         {myData[index]}
+//       </div>
+//     );
+//   }
   
-  return(
+//   return(
     
-    <AutoSizer>
-      {({height, width}) => (
-        <List
-          height={height}
-          rowCount={myData.length}
-          rowHeight={1000}
-          rowRenderer={rowRenderer}
-          width={width}
-        />
-      )}
-    </AutoSizer>
-  )
-}
+//     <AutoSizer>
+//       {({height, width}) => (
+//         <List
+//           height={height}
+//           rowCount={myData.length}
+//           rowHeight={1000}
+//           rowRenderer={rowRenderer}
+//           width={width}
+//         />
+//       )}
+//     </AutoSizer>
+//   )
+// }
 
 
 
---
+++ src/hooks/useColorFleeter.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+export const useColorFleeter = (propNumber) => {
+    const prevLocalNumberRef = useRef();
+    const [localNumber, setLocalNumber] = useState(0)
+    
+    const colorDetector = () => {
+        let color = ''
+        if(localNumber > prevLocalNumberRef.current) color = 'green'
+        else if(localNumber < prevLocalNumberRef.current) color = 'red'
+        else color = 'black'
+        return color
+    }
+
+    useEffect(() => {
+        prevLocalNumberRef.current = localNumber;
+        setLocalNumber(propNumber)
+    }, [propNumber])
+
+    return colorDetector()
+}
