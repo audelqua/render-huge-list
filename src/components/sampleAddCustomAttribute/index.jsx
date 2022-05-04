0777ed5699d698f911726beefa03a7692ed77a47
audrey <mbozorgi604@gmail.com>
2022-05-04 20:42:16 +0430

    get custom attribute amound

--- src/components/sampleAddCustomAttribute/index.jsx
+++ src/components/sampleAddCustomAttribute/index.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 const SampleAddCustomAttribute = props => {
+    const myRef = useRef()
 
+    useEffect(() => {
+        console.log(myRef.current.getAttribute('data-name'));
+        console.log(document.getElementById('custom_div').getAttribute('customName'));
+    },[])
+    console.log('document', document);
     return (
-        <div>
+        <div customName='hello' id='custom_div' ref={myRef}>
             hi custom
         </div>
     )
