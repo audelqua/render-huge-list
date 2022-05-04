3c3aca72e2021130339eb7604fb7477c434419b7
audrey <mbozorgi604@gmail.com>
2022-05-04 20:46:45 +0430

    define data- attribute

--- src/components/sampleAddCustomAttribute/index.jsx
+++ src/components/sampleAddCustomAttribute/index.jsx
@@ -4,12 +4,13 @@ const SampleAddCustomAttribute = props => {
     const myRef = useRef()
 
     useEffect(() => {
-        console.log(myRef.current.getAttribute('data-name'));
+        console.log(myRef.current);
         console.log(document.getElementById('custom_div').getAttribute('customName'));
+        console.log(document.getElementById('custom_div').getAttribute('data-number'));
     },[])
     console.log('document', document);
     return (
-        <div customName='hello' id='custom_div' ref={myRef}>
+        <div customName='hello' id='custom_div' ref={myRef} data-number={22}>
             hi custom
         </div>
     )
