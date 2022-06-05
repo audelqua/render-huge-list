1ea43298b929099960f007ef0c62a7abdcf452c7
audrey <mbozorgi604@gmail.com>
2022-06-06 02:03:54 +0430

    log removed

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -2,7 +2,6 @@ import React, { useState, useCallback, useMemo } from 'react'
 
 const useToggle = () => {
     const [bool, setBool] = useState(false)
-    console.log('ssss', bool);
     const handleToggle = useCallback(() => setBool(prevBool => !prevBool), [])
 
     const values = useMemo(
