8a5657d3b0c3c865da942b61743aced048ae8fda
audrey <mbozorgi604@gmail.com>
2022-09-26 17:40:00 +0330

    playing around with some other selectord

--- src/components/cssSelectors/index.js
+++ src/components/cssSelectors/index.js
@@ -13,10 +13,28 @@ const CssSelector = () => {
                     <span>child div 2</span>
                 </div>
                 <div className='general-child child-3'>
-                    <span>child div 3</span>
+                    <span className='span-child-3'>child div 3</span>
+                    <p>hellooooo</p>
                 </div>
                 <div className='child-4'>
                     <span>child div 4</span>
+                </div>
+                <div className='child-5'>
+                    <span>child div 5</span>
+                </div>
+                <div className='child-6'>
+                    <span>child div 6</span>
+                </div>
+                <div className='child-7'>
+                    <span>child div 7</span>
+                </div>
+                <div className='child-8'>
+                    <span>
+                        hello
+                        <div>
+                            <span>child div 8</span>
+                        </div>
+                    </span>
                 </div>
             </div>
             <div className='container-2'>
@@ -27,10 +45,10 @@ const CssSelector = () => {
                     <span>child div 2</span>
                 </div>
                 <div className='general-child child-3'>
-                    <span>child div 3</span>
+                    <span className='span-child-3'>child div 3</span>
                 </div>
-                <div className='child-4'>
-                    <span>child div 4</span>
+                <div className='child-7'>
+                    <span>child div 7</span>
                 </div>
             </div>
         </>
