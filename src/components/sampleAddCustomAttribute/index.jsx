9643e925f67773504ea632c6f9247307bafa5795
audrey <mbozorgi604@gmail.com>
2022-11-26 01:13:19 +0330

    A commit in feature beranch

--- src/components/sampleAddCustomAttribute/index.jsx
+++ src/components/sampleAddCustomAttribute/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 const SampleAddCustomAttribute = props => {
+    //this is A commit in feature branch
     const myRef = useRef()
 
     useEffect(() => {
