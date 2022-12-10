3b66d4160cb479c17157fb5a264ea89a5e0a7df0
audrey <mbozorgi604@gmail.com>
2022-12-10 17:56:53 +0330

    working on sort funciton

--- src/components/SimpleArray/index.jsx
+++ src/components/SimpleArray/index.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from 'react'
-
+// writing custom sort by hand
 const ShoppingCart = () => {
+    const [array, setArray] = useState([7,1,5,3])
+
+    const handleCallReorderFunction = () => {
+        let tempArray = Array.from(array)
+
+        for(let i = 0; i < tempArray.length; i++) {
+            for(let j = 0; j < i; j++){
+                console.log('j', j);
+            }
+        }
+    }
     
     return(
         <div>
-           
+            <ol>
+                {array.map((item, index) => 
+                    <li key={item}>
+                        {item}
+                    </li>
+                )}
+            </ol>
+            <button onClick={handleCallReorderFunction}>call reorder</button>
         </div>
     )
 }
