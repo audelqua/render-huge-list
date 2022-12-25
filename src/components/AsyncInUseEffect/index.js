6118fcb98d8ca8d3da241c99df7b99d033c1c922
audrey <mbozorgi604@gmail.com>
2022-12-26 00:25:11 +0330

    async funtion reference inside regular callback for useEffect

--- src/components/AsyncInUseEffect/index.js
+++ src/components/AsyncInUseEffect/index.js
@@ -1,15 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react'
 
-let someInterval 
+// let someInterval 
 const AsyncInUseEffect = () => {
 
+    // useEffect(() => {
+    //     someInterval = setInterval(() => console.log('called'), 1000)
+    //     return function() {
+    //         console.log('component will unmount');
+    //         clearInterval(someInterval)
+    //     }
+    // },[])
+
+    const someFunction = async() => {
+        let res = await new Promise((res, rej) => {
+            setTimeout(() => {
+                    res('im resolve after 4 seconds')
+            }, 4000)
+        })
+        console.log('res is: ', res);
+    }
+
     useEffect(() => {
-        someInterval = setInterval(() => console.log('called'), 1000)
-        return function() {
-            console.log('component will unmount');
-            clearInterval(someInterval)
+        someFunction()
+        return () => {
+            console.log('sdsdsd');
         }
-    },[])
+    }, [])
 
     return (
         <div>
