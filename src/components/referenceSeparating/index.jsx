ce45dcdefcb1c67c70d1af9f5595581edb8afef5
audrey <mbozorgi604@gmail.com>
2023-01-08 19:41:27 +0330

    add lodash and do deep cloning

--- src/components/referenceSeparating/index.jsx
+++ src/components/referenceSeparating/index.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import _ from 'lodash'
 
 const ReferenceSeparating = () => {
+    const someObject = {
+        firstCustomKey: {
+            secondCustomKey: {
+                thirdCustomKey: {
+                    fourthCustomKey: {
+                        hello: 'im audrey'
+                    }
+                }
+            }
+        }
+    }
+
+    const anotherObject = _.cloneDeep(someObject) //lodash does deep clone and separate references deeply.
+
+
+    anotherObject['firstCustomKey']['secondCustomKey']['thirdCustomKey']['fourthCustomKey']['hello'] = 2
+
+    console.log('someObject', someObject);
+    console.log('anotherObject', anotherObject);
+    
     return (
         <div>
             reference 
