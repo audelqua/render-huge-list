2f099a1a9de512d4e3c87e34c32307229d483e38
audrey <mbozorgi604@gmail.com>
2023-01-10 19:52:11 +0330

    useMemo implemented

--- src/components/sampleOfMemo/index.js
+++ src/components/sampleOfMemo/index.js
@@ -1,48 +1,75 @@
 // sample with useReducer
-import React, { useCallback, useEffect, useState, memo } from 'react'
+import React, { useCallback, useEffect, useState, memo, useMemo } from 'react'
 
 const SampleOfMemo = props => {
-    const [state, setState] = useState(0)
-    const [anotherState, setAnotherState] = useState(0)
+    const [counter, setCounter] = useState(0)
+    const someStoredValue = useMemo(() => someMassiveCalculator(), []) 
 
-    const handleClick = useCallback(() => {
-        console.log('hi im in handleClick');
-        setState(state => state + 1)
-    }, [])
-
-    const anotherCallbackNotRelatedToChild = () => {
-        console.log('hi im in another handle click');
-        setAnotherState(prev => prev + 1)
+    const handleClick = () => {
+        setCounter(prev => prev + 1)
     }
 
     return (
-        <div>
-            hi there im memoized component
-            <ExpensiveChildComponent handleClick={handleClick} state={state}/>
-            <button onClick={anotherCallbackNotRelatedToChild}>anotherCallbackNotRelatedToChild</button>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+            hi there im massive data: {someStoredValue}
+            <button onClick={handleClick}>another function updating some unrelated state</button>
+            counter is: {counter}
         </div>
     )
 }
 export default SampleOfMemo
 
-const ExpensiveChildComponent = memo(({handleClick, state}) => {
-    const massiveCalculation = () => {
-        let tempVar
-        for(let i = 0; i < 1000000000; i ++){
-            tempVar = i
-        }
-        console.log('i did my calc', tempVar);
+const someMassiveCalculator = () => {
+    let tempVar = 0
+    for(let i = 0; i< 1000000000; i++) {
+        tempVar = i 
     }
+    console.log('calc is done');
+    return tempVar
+}
 
-    useEffect(() => {
-        massiveCalculation()
-    })
-    return (
-        <div>
-            im expensive child
-            <button onClick={handleClick}>handleClick</button>
-            {state}
-        </div>
-    )
-})
+
+
+// const handleClick = useCallback(() => {
+//     console.log('hi im in handleClick');
+//     setState(state => state + 1)
+// }, [])
+
+// const anotherCallbackNotRelatedToChild = () => {
+//     console.log('hi im in another handle click');
+//     setAnotherState(prev => prev + 1)
+// }
+
+// const expensiveCalInParent = useMemo(() => {
+//     let tempVar
+//     for(let i = 0; i < 1000000000; i ++){
+//         tempVar = i
+//     }
+//     console.log('im ready to show everything');
+//     return tempVar
+// }, [])
+
+
+
+{/* <ExpensiveChildComponent handleClick={handleClick} state={state}/> */}
+// const ExpensiveChildComponent = memo(({handleClick, state}) => {
+//     const massiveCalculation = () => {
+//         let tempVar
+//         for(let i = 0; i < 1000000000; i ++){
+//             tempVar = i
+//         }
+//         console.log('i did my calc', tempVar);
+//     }
+
+//     useEffect(() => {
+//         massiveCalculation()
+//     })
+//     return (
+//         <div>
+//             im expensive child
+//             <button onClick={handleClick}>handleClick</button>
+//             {state}
+//         </div>
+//     )
+// })
 
